329b440f041c39cbb64d3421f872537035143bad
Samueelx <gsammygitau@gmail.com>
2023-01-09 00:50:41 +0300

    managed to get 18 tests passing.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+const createEmployeeRecord = (array) => {
+    const record = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return record;
+}
+
+const createEmployeeRecords = (twoRows) => {
+    const employeeRecords = [];
+    twoRows.forEach((array) => {
+        employeeRecords.push(createEmployeeRecord(array));
+    });
+    return employeeRecords;
+}
+const createTimeInEvent = function(employeeRecord, datestamp) {
+    const timeIn = {
+        type: "TimeIn",
+        date: datestamp.slice(0, 10),
+        hour: parseInt(datestamp.slice(11))
+    };
+    employeeRecord.timeInEvents.push(timeIn);
+    return employeeRecord;
+}
+
+// const Thor = createEmployeeRecord(["Thor", "Odinsson", "Electrical Engineer", 45]);
+// const updatedThor = createTimeInEvent(Thor, "2014-02-28 1400");
+// console.log(updatedThor);
